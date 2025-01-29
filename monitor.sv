cfc389f8c5337f9f24b9e08677d9e9c022cd2640
prasannakumar14 <palevallakumar@gmail.com>
2025-01-29 16:40:01 +0530

    added build phase

--- monitor.sv
+++ monitor.sv
@@ -2,4 +2,8 @@ class monitor extends uvm_monitor;
 	function new(string name="monitor", uvm_component parent);
 		super.new(name,parent);
 	endfunction
+
+	function void build_phase(uvm_phase phase);
+	    super.build_phase(phase);
+	endfunction
 endclass
