7aaa7799585b9ab20bdc33ed440c1537561b0777
prasannakumar14 <palevallakumar@gmail.com>
2025-01-29 16:29:21 +0530

    added build phase

--- driver.sv
+++ driver.sv
@@ -1,5 +1,10 @@
 class driver extends uvm_driver;
 	function new(string name="driver", uvm_component parent);
-	super.new(name,parent);
-endfunction
+	  super.new(name,parent);
+    endfunction
+
+	function void build_phase(uvm_phase phase);
+	  super.build_phase(phase);
+	endfunction
+
 endclass
